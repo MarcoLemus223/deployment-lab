0bb798e267745719dcfc2c7db488cd4c00f403e8
Marco Lemus <marcoantonio_lemus@yahoo.com>
2022-06-29 16:20:47 -0600

    updated more

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const bigButton = document.querySelector('#dontPush')
+const cartButton = document.querySelector('#cart')
+const cartButtonTwo = document.querySelector('#cartTwo')
+const cartButtonThree = document.querySelector('#cartThree')
 
 const message = () => {
     alert('I told you not to push me')
 }
 
+const orderAdded = () => {
+    alert('Thank you for your purchase! An email has been sent')
+}
+
 bigButton.addEventListener('click', message)
+cartButton.addEventListener('click', orderAdded)
+cartButtonTwo.addEventListener('click', orderAdded)
+cartButtonThree.addEventListener('click', orderAdded)
